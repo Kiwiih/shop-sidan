f0b20e3d092b4c5928950fbdd9ab871e55a011ae
ibrahimovic <144237765+ibrahims09@users.noreply.github.com>
2023-12-08 11:05:29 +0100

    la till rullgardiner

---
+++ filter.js
@@ -0,0 +1,17 @@
+const products = [
+  { id: 1, name: 'T-shirt', category: 'kläder', price: 100 },
+  { id: 2, name: 'Hörlurar', category: 'elektronik', price: 250 },
+  { id: 3, name: 'Keps', category: 'kläder', price: 50 },
+  { id: 4, name: 'Mobiltelefon', category: 'elektronik', price: 500 }
+];
+
+const result = products.filter(product => true);
+
+const document = addEventListener(product ({
+  
+
+
+}));
+
+
+
